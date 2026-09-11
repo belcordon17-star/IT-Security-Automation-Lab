a9882f24bb1efb565a18d80e00fb69e5ce393ade
belcordon17-star <bel.cordon17@gmail.com>
2026-09-11 13:45:23 -0700

    Update ip_discovery .py

--- python/ip_discovery .py
+++ python/ip_discovery .py
@@ -1,1 +1,6 @@
 print("hello network")
+devices = 192.168.1.1
+
+for ip in devices result = subprocess.run
+
+1", ip capture_output = True, text = True status = "online" if result .returncode == 0 else "offline" print(f'{ip}: {status}')
